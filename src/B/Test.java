b36d9231f055c838e51055e75ad82ae51c9d02bf
Q-later <1875513983@qq.com>
2018-02-24 21:38:43 +0800

    1

---
+++ src/B/Test.java
@@ -0,0 +1,13 @@
+package B;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       paixu pa=new paixu();
+       pa.Setname("Àî°×");
+       
+      System.out.println(pa.getname());
+	}
+
+}
---
+++ src/B/paixu.java
@@ -0,0 +1,19 @@
+package B;
+
+public class paixu {
+     private String name;
+     private int age;
+     private char sex;
+	
+     public void Setname(String name) {
+    	 this.name=name;	 
+     }
+     public String getname() {
+    	 return name;	 
+     }
+     public  void method() {
+    	 System.out.println("我是："+name+"今年"+age+"岁了");
+    	 
+    	 
+     }
+}
